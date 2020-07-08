fa7a067004a2895a846d83c23a1f465e14d4d88b
ThomasTog <hrc720@alumni.ku.dk>
2020-07-08 22:10:50 +0200

    Basic GET usage

--- CluedInAssignment/Program.cs
+++ CluedInAssignment/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using RestSharp;
+using Newtonsoft.Json;
 
 namespace CluedInAssignment
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string domain = "https://my.api.mockaroo.com";
+            string resource = "cluedin_assignment_2020.json";
+            string token = "91bc8540";
+
+            var client = new RestClient(domain);
+            client.AddDefaultHeader("x-api-key", token);
+
+            var request = new RestRequest(resource);
+            var response = client.Get(request);
+
+            var thing = JsonConvert.DeserializeObject<object>(response.Content);
         }
     }
 }
